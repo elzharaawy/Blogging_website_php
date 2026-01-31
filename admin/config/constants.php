64cef8ab0daa352839db7eeba2eb6ebddb213b5f
Abdirahman Saed <167611318+elzharaawy@users.noreply.github.com>
2026-01-31 20:22:34 +0530

    Add files via upload

--- admin/config/constants.php
+++ admin/config/constants.php
@@ -1,1 +1,17 @@
+<?php
+session_start();
 
+define("ROOT_URL", "http://localhost/blogwebsite/");
+define('DB_HOST', 'localhost');
+define('DB_USER', 'elzharaawy');
+define('DB_PASS', 'NAAMegah123');
+define('DB_NAME', 'blogdatabase');
+
+
+if (!isset($_SESSION['user-id'])) {
+    header("location: " . ROOT_URL . "logout.php");
+    //destroy all sessions and redirect user to login page
+    session_destroy();
+    die();
+    header("location: " . ROOT_URL . "signin.php");
+}
