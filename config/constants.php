23efbadcbc66c42342013c1f00e82fb8e964f740
Abdirahman Saed <167611318+elzharaawy@users.noreply.github.com>
2026-02-01 18:56:29 +0530

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -2,20 +2,21 @@
 
 // Check if constants are already defined before defining them
 if (!defined('ROOT_URL')) {
-  define('ROOT_URL', 'http://localhost/blogwebsite/');
+    define('ROOT_URL', 'http://localhost/blogwebsite/');
 }
+
 if (!defined('DB_HOST')) {
     define('DB_HOST', 'localhost');
 }
 
 if (!defined('DB_USER')) {
-    define('DB_USER', 'elzharaawy');
+    define('DB_USER', 'your_database_username');
 }
 
 if (!defined('DB_PASS')) {
-    define('DB_PASS', 'NAAMegah123');
+    define('DB_PASS', 'your_database_password');
 }
 
 if (!defined('DB_NAME')) {
-    define('DB_NAME', 'blogdatabase');
+    define('DB_NAME', 'your_database_name');
 }
